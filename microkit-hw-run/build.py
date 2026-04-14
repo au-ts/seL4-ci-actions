b121008afc408d4f7d0da427dd904b9f00006cf9
Julia Vassiliki <julia.vassiliki@unsw.edu.au>
2026-04-14 18:16:55 +1000

    add build
    Signed-off-by: Julia Vassiliki <julia.vassiliki@unsw.edu.au>

--- microkit-hw-run/build.py
+++ microkit-hw-run/build.py
@@ -7,12 +7,12 @@
 Parse builds.yml and run sel4test hardware builds and runs on each of the build definitions.
 
 Expects seL4-platforms/ to be co-located or otherwise in the PYTHONPATH.
-Expects microkit/build_sdk.py to be co-located or otherwise in the PYTHONPATH
+Expects TEST_CASES environment variable to be a JSON.
 """
 
-from builds import Build, run_build_script, run_builds, load_builds, junit_results
+from builds import Build, run_build_script, run_builds, filtered
 from builds import release_mq_locks, SKIP
-from platforms import Platform, gh_output
+from platforms import Platform
 
 from pprint import pprint
 from typing import List
@@ -20,6 +20,11 @@ from typing import List
 import json
 import os
 import sys
+
+# class MicrokitBuild(Build):
+#     def get_platform(self) -> Platform:
+#         return microkit_platforms
+
 
 def hw_run(manifest_dir: str, build: Build) -> int:
     """Run one hardware test."""
@@ -33,28 +38,47 @@ def hw_run(manifest_dir: str, build: Build) -> int:
     return run_build_script(manifest_dir, build, script, final_script=final, junit=False)
 
 
-def verification_equals_release(build: Build) -> bool:
-    """Return whether in this build release and verification settings are equivalent."""
-
-    plat = build.get_platform()
-    # TX2 currently still set AArch64SErrorIgnore=ON for release mode
-    return plat.arch == 'riscv' or (plat.arch == 'arm' and not plat.name == 'TX2')
 
 
 def hw_test_filter(build: Build) -> bool:
     plat = build.get_platform()
 
-    if plat.no_hw_build:
+    if plat.no_hw_test:
         return False
 
     return True
 
 
+def load_builds_microkit(filter_fun=lambda x: True) -> List[Build]:
+    test_cases: list[dict] = os.environ["TEST_CASES"]
+
+    env_filters = get_env_filters()
+
+    DEFAULTS = {
+        "success": "Test",
+    }
+
+    [Build(b, DEFAULTS) for b in yml_builds]
+
+    builds = []
+    for test_case in tests_cases:
+        platform = test_case["platform"]
+        config = test_case["config"]
+        march = test_case["march"]
+
+        build = Build({ "platform" : platform })
+
+        build = build if filter_fun(build) else None
+        build = filtered(build, env_filters)
+        if build:
+            builds.append(build)
+
+    return builds
+
 
 # If called as main, run all builds from builds.yml
 if __name__ == '__main__':
-    # builds = load_builds(os.path.dirname(__file__) + "/builds.yml", filter_fun=hw_test_filter)
-    builds = []
+    builds = load_builds_microkit(filter_fun=hw_test_filter)
 
     if len(sys.argv) > 1 and sys.argv[1] == '--hw':
         sys.exit(run_builds(builds, hw_run))
