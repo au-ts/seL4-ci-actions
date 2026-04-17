b078fbdd67c4e243c5ee72eb1ae7a98070589b78
Julia Vassiliki <julia.vassiliki@unsw.edu.au>
2026-04-17 16:22:18 +1000

    fix env filter
    Signed-off-by: Julia Vassiliki <julia.vassiliki@unsw.edu.au>

--- seL4-platforms/builds.py
+++ seL4-platforms/builds.py
@@ -777,8 +777,8 @@ def filtered(build: Build, build_filters: dict) -> Optional[Build]:
             elif k in ['name', 'app']:
                 if vars(build).get(k) not in v:
                     return False
-            elif hasattr(build, k):
-                if getattr(build, k) not in v:
+            elif vars(build).get(k, None) is not None:
+                if vars(build).get(k) not in v:
                     return False
             elif not vars(build.get_platform()).get(k):
                 return False
